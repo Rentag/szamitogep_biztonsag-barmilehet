1dfcce24d9070820b5460d0c54062cd362c79f51
Horváth Gergő <h.gery94@gmail.com>
2020-11-08 11:45:38 +0100

    desktop application
    desktop application to generate a .ciff file from a .caff file.

---
+++ src/CaffToCiff.cpp
@@ -0,0 +1,73 @@
+// CaffToCiff.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+#pragma pack(push)
+#pragma pack(1) //pragma pack to match an EXACT data layout
+struct CaffHeader {
+    char magic[4]; // 4 bytes
+    __int64 h_size; // 8 bytes
+    __int64 count; // 8 bytes
+};
+#pragma pack(pop)
+
+#pragma pack(push)
+#pragma pack(1)
+struct CaffBlock {
+    __int8 blockType; // 1 byte
+    __int64 blockSize; // 8 bytes
+};
+#pragma pack(pop)
+
+void generateCiff(const char* caffSrc, const char* ciffDst) {
+
+    ifstream is(caffSrc, ios::binary | ios::in);
+    if (!is.is_open()) { cerr << "File not found" << endl; exit(EXIT_FAILURE); }
+    ofstream os(ciffDst, ios::binary | ios::out);
+    if (!os.is_open()) { is.close(); cerr << "File could not be created" << endl; exit(EXIT_FAILURE); }
+
+    CaffBlock cf; // 9 byte
+    CaffHeader ch; // 20 byte
+    bool readBlocks = true;
+
+    if (!is.read((char*)&cf, sizeof(cf))) { cerr << "Failed to read metadata" << endl; readBlocks = false; } // read block metadata // in1.ciff 0x1 - header
+
+    if (!is.read((char*)&ch, sizeof(ch))) { cerr << "Failed to read header block" << endl; readBlocks = false; } // read header block
+
+    if (strncmp(ch.magic, "CAFF", 4)) { cerr << "Wrong file format" << endl; readBlocks = false; } // magic is not null-terminated
+    
+    while (readBlocks) {
+        if (!is.read((char*)&cf, sizeof(cf))) { cerr << "Failed to read metadata" << endl; break; } // read block metadata // in1.coff 0x2 credentials or 0x3 animation
+        
+        switch (cf.blockType)
+        {
+        case 0x2:
+            is.seekg(cf.blockSize, ios::cur);
+            break;
+        case 0x3:
+            copy_n(istreambuf_iterator<char>(is.seekg(8, ios::cur)), cf.blockSize - 8, ostreambuf_iterator<char>(os)); // copy the content of ciff into .ciff file
+            readBlocks = false;
+            break;
+        default:
+            cerr << "invalid block type" << endl;
+            readBlocks = false;
+            break;
+        }
+    }
+
+    is.close();
+    os.close();
+}
+
+int main(int argc, char* argv[])
+{
+    const char* caffSrc = argv[1];
+    const char* ciffDst = argv[2];
+
+    generateCiff(caffSrc, ciffDst);
+}
