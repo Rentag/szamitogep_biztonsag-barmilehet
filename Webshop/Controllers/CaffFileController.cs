accf89dc05527141a8c4ff30adc30f7cbfb22423
Timkó Ákos <playmobilo97@gmail.com>
2020-11-19 23:11:06 +0100

    phy del, upload, download

--- Webshop/Controllers/CaffFileController.cs
+++ Webshop/Controllers/CaffFileController.cs
@@ -5,13 +5,16 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Webshop.Authorization;
 using Webshop.Data;
 using Webshop.Models;
 using Webshop.ViewModels.CaffFileViewModels;
+using Ciff;
 
+using SixLabors.ImageSharp;
 namespace Webshop.Controllers
 {
     [Authorize(Roles = "Admin,User")]
@@ -22,6 +25,19 @@ namespace Webshop.Controllers
         private readonly UserManager<SiteUser> _userManager;
         private readonly IAuthorizationService _authorizationService;
 
+        //for caff validation
+        private string[] permittedExtensions = { ".caff" };
+        private static readonly Dictionary<string, List<byte[]>> _fileSignature =
+                                                new Dictionary<string, List<byte[]>>
+        {
+            { ".caff", new List<byte[]>
+                {
+                    new byte[] { 0x43, 0x41, 0x46, 0x46}
+                }
+            },
+        };
+        //20MB
+        private static long fileSizeLimit = 20000 * 1024;
 
         public CaffFileController(WebshopContext context, UserManager<SiteUser> userManager, IAuthorizationService authorizationService)
         {
@@ -73,30 +89,103 @@ namespace Webshop.Controllers
                 {
                     return Unauthorized();
                 }
-
+                
                 if (caff.Content != null)
                 {
                     // TODO: Add the file uploader function here that returns the path of the uploaded CAFF file
-                    var caffPath = VerifyAndUploadCaffFile(caff.Content);
+                    var verified = VerifyAndUploadCaffFileAsync(caff.Content);
+                    var caffPath = verified[0];
+                    if (caffPath == "")
+                    {
+                        return View("Error");
+                    }
                     // TODO: Process the CAFF file and get the result CIFF
                     // TODO: Add the CIFF to PNG converter function here that returns the path of the generated image
-                    var imagePath = "pathFromConverterFunction";
+                    var imagePath = verified[1];
 
                     newCaffFile.Path = caffPath;
                     newCaffFile.ImagePath = imagePath;
                     _context.CaffFiles.Add(newCaffFile);
+                   
                     await _context.SaveChangesAsync();
 
                 }
+                
                 return RedirectToAction(nameof(Index));
 
             }
             return View(caff);
         }
 
-        public String VerifyAndUploadCaffFile(IFormFile caff)
+        public  List<string> VerifyAndUploadCaffFileAsync(IFormFile formFile)
         {
-            return "pathFromUploaderFunction";
+            var filePaths = new List<string>();
+            var ext = Path.GetExtension(formFile.FileName).ToLowerInvariant();
+            bool isvalid = true;
+            if (string.IsNullOrEmpty(ext) || !permittedExtensions.Contains(ext))
+            {
+                isvalid = false;
+                ViewBag.Failure = "File upload was unsuccessful due to not appropriate file extension.";
+              //  return View("Error");
+            }
+
+            //Check Magic bytes
+            using (var reader = new BinaryReader(formFile.OpenReadStream()))
+            {
+                var signatures = _fileSignature[ext];
+                var all_headerBytes = reader.ReadBytes(signatures.Max(m => m.Length + 9));
+                byte[] headerBytes = new byte[signatures[0].Length];
+                for (int i = 0; i < all_headerBytes.Length; i++)
+                {
+                    if (i > 8)
+                    {
+                        headerBytes[i - 9] = all_headerBytes[i];
+                    }
+                }
+                
+                if (!(signatures.Any(signature =>
+                    headerBytes.Take(signature.Length + 9).SequenceEqual(signature))))
+                {
+                    isvalid = false;
+                    ViewBag.Failure = "File upload was unsuccessful due to not appropriate magic numbers.";
+                    //return View("Error");
+                }
+            }
+            //Check if not null and length is maximumom of x bytes.
+
+
+            if (formFile.Length > 0 && formFile.Length <= fileSizeLimit)
+            {
+                if (isvalid)
+                {
+                    var random = Path.GetRandomFileName();
+                    var replaced = random.Split(".")[0] + ".caff";
+                    
+                    //filePaths[0] - CAFF PATH
+                    //filePaths[1] - PNG PATH
+                    filePaths.Add(Path.Combine("\\Uploads\\CAFF\\", replaced));
+                    var caffToCiff = "dll";
+                    var path = "path";
+
+                    //TODO: DYNAMIC
+                    filePaths.Add("\\images\\uploads\\"+ random.Split(".")[0] + ".png");
+                    var ciffToPng = new ciff();
+                    
+                    ciffToPng.cifftopng(Directory.GetCurrentDirectory()+"\\Uploads\\CIFF\\something.ciff",
+                                    Directory.GetCurrentDirectory() + "\\wwwroot\\images\\uploads\\" + random.Split(".")[0] + ".png");
+                    using (var stream = System.IO.File.Create(Directory.GetCurrentDirectory() + filePaths[0]))
+                    {
+                        formFile.CopyToAsync(stream);
+                    }
+                    //return filePaths;
+                }
+            }
+            else
+            {
+                ViewBag.Failure = "File upload was unsuccessful due to not appropriate file size.";
+                //return View("Error");
+            }
+            return filePaths;
         }
 
         public IActionResult Edit(int? id)
@@ -158,12 +247,15 @@ namespace Webshop.Controllers
             {
                 return NotFound();
             }
-
+            
             var isAuthorized = _authorizationService.AuthorizeAsync(User, caffToDelete, CaffFileOperations.Delete);
             if (!isAuthorized.Result.Succeeded)
             {
                 return Unauthorized();
             }
+            //string[] files = Directory.GetFiles("");
+           
+
 
             return View(caffToDelete);
         }
@@ -184,6 +276,18 @@ namespace Webshop.Controllers
             {
                 return Unauthorized();
             }
+            var paths = new List<string>();
+            
+            //Delete file physically
+            paths.Add(originalCaffFile.ImagePath);
+            paths.Add(originalCaffFile.Path);
+            paths.Add(originalCaffFile.Path.Replace("CAFF", "CIFF"));
+            System.IO.File.Delete(Directory.GetCurrentDirectory()+ "\\wwwroot" + paths[0]);
+            System.IO.File.Delete(Directory.GetCurrentDirectory() + paths[1]);
+            System.IO.File.Delete(Directory.GetCurrentDirectory() + paths[2]);
+            
+            
+            
             _context.CaffFiles.Remove(originalCaffFile);
             await _context.SaveChangesAsync();
             // TODO: Delete CAFF and PNG files as well
@@ -198,8 +302,38 @@ namespace Webshop.Controllers
             return _context.CaffFiles
                 .FirstOrDefault(Caff => Caff.Id == caffId);
         }
-        
-        // TODO: Download CAFF file
 
+        // TODO: Download CAFF file
+        public async Task<IActionResult> Download(int? id)
+        {
+            if (id == null)
+                return View("Error");
+
+           
+            var path =  Directory.GetCurrentDirectory() + GetCaffFile(id).Path;
+
+            var memory = new MemoryStream();
+            using (var stream = new FileStream(path, FileMode.Open))
+            {
+                await stream.CopyToAsync(memory);
+            }
+            memory.Position = 0;
+            return File(memory,GetContentType(path), Path.GetFileName(path));
+        }
+
+        private string GetContentType(string path)
+        {
+            var types = GetMimeTypes();
+            var ext = Path.GetExtension(path).ToLowerInvariant();
+            return types[ext];
+        }
+
+        private Dictionary<string, string> GetMimeTypes()
+        {
+            return new Dictionary<string, string>
+            {
+                {".caff", "text/plain"}
+            };
+        }
     }
 }
