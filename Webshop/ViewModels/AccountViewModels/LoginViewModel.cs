71c455cdbbc870e457c3b569161ae50f98311b8a
Finta Norbert <norbi.f95@gmail.com>
2020-11-29 20:00:34 +0100

    Add regular expression check for user login

--- Webshop/ViewModels/AccountViewModels/LoginViewModel.cs
+++ Webshop/ViewModels/AccountViewModels/LoginViewModel.cs
@@ -5,6 +5,7 @@ namespace Webshop.Models.AccountViewModels
     public class LoginViewModel
     {
         [Required(ErrorMessage = "You must enter a valid username.")]
+        [RegularExpression("^[A-Za-z0-9_-]{5,40}$", ErrorMessage = "Invalid username")]
         public string UserName { get; set; }
 
         [Required(ErrorMessage = "You must enter a valid password.")]
